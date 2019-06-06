382f24db528e88c3041ad253d4cd6e411c7887fe
tyj <420785022@qq.com>
2019-06-06 20:21:53 +0800

    微信小程序登录，jedis

--- src/main/java/com/rogy/smarte/controller/client/LoginController.java
+++ src/main/java/com/rogy/smarte/controller/client/LoginController.java
@@ -1,31 +1,35 @@
 package com.rogy.smarte.controller.client;
 
+import com.alibaba.fastjson.JSONObject;
 import com.rogy.smarte.entity.db1.User;
 import com.rogy.smarte.entity.db1.UserKey;
 import com.rogy.smarte.repository.db1.UserDao;
 import com.rogy.smarte.resp.ResponseCode;
 import com.rogy.smarte.resp.ServerResponse;
 import com.rogy.smarte.service.IUserRolePermissionsService;
+import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.hibernate.service.spi.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @RestController
 public class LoginController {
@@ -152,4 +156,92 @@ public class LoginController {
     public ServerResponse kickOut() {
         return ServerResponse.customError(ResponseCode.UNAUTHORIZED);
     }
+
+    @CrossOrigin
+    @RequestMapping(value = "/AppClientAction_miniLogin.do", method = RequestMethod.GET)
+    public void loginByOpenId(String js_code) {
+
+        final String fuwuAppid = "wx718ec2dbc3762994";
+        final String fuwuSecret = "44896e5be0d36b1bb8891d9082d9987c";
+
+        String access_token;//token
+        String openId;//openid
+        String UnionID;//UnionID
+        Integer expires_in;//token 过期时间
+
+
+        //校验数据的合法性,是否为空
+        if (StringUtils.isBlank(js_code)) {
+            throw new ServiceException("code empty");
+        }
+
+
+        //调用微信接口获取
+        RestTemplate restTemplate = new RestTemplate();
+        //获取openId
+        String httpOpenIdUrl = "https://api.weixin.qq.com/sns/jscode2session?appid=" + fuwuAppid + "&secret=" + fuwuSecret + "&js_code=" + js_code + "&grant_type=authorization_code";
+        ResponseEntity<String> response = restTemplate.getForEntity(httpOpenIdUrl, String.class);
+        if (200 != response.getStatusCodeValue()) {
+            logger.error("请求失败，获取openId失败");
+        }
+        String r = response.getBody();
+        JSONObject body = JSONObject.parseObject(r);
+        if ((Integer) body.get("errcode") != null) {
+            logger.error("请求成功，但是获取openId失败，错误码：" + body);
+        }
+        //网页授权token 不一定能用得上
+        //access_token = (String) body.get("access_token");
+        openId = (String) body.get("openid");
+
+        JedisPool jedisPool = new JedisPool("192.168.3.19");
+        //获取基础access_token
+        try (Jedis jedis = jedisPool.getResource()) {
+//            jedis.select(CommonConstant.REDIS_YANCODE);
+            access_token = jedis.get("access_token");
+
+            /** 不存在则重新去请求*/
+            if (StringUtils.isBlank(access_token)) {
+
+                //获取access_token
+                String httpTokenUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + fuwuAppid + "&secret=" + fuwuSecret;
+                ResponseEntity<Map> response2 = restTemplate.getForEntity(httpTokenUrl, Map.class);
+                if (200 != response2.getStatusCodeValue()) {
+                    logger.error("请求失败，获取access_token失败");
+                }
+                Map body2 = response2.getBody();
+                if ((Integer) Objects.requireNonNull(body2).get("errcode") != null) {
+                    logger.error("请求成功，但是获取access_token失败，错误码：" + body2.get("errcode"));
+                }
+                access_token = (String) body2.get("access_token");
+                expires_in = (Integer) body2.get("expires_in");
+
+
+                //把新的access_token放入redis缓存
+                jedis.set("access_token", access_token);
+                jedis.expire("access_token", expires_in);
+
+
+            }
+        }
+
+        //获取unionId
+        String httpUnionIDUrl = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=" + access_token + "&openid=" + openId + "&lang=zh_CN";
+        ResponseEntity<String> response1 = restTemplate.getForEntity(httpUnionIDUrl, String.class);
+        if (200 != response1.getStatusCodeValue()) {
+            logger.error("请求失败，获取UnionId失败");
+        }
+        String s = response1.getBody();
+        JSONObject body1 = JSONObject.parseObject(s);
+
+        if ((Integer) body1.get("errcode") != null) {
+            logger.error("请求成功，但是获取UnionId失败，错误码：" + body1);
+        }
+        UnionID = (String) body1.get("unionid");
+        //返回前端数据
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("UnionID", UnionID);
+
+        System.out.println(map);
+    }
+
 }
